7562000c7faa71ac41b08336fbcfed17d4a58bf8
Kaushik-Nishant <116556079+Kaushik-Nishant@users.noreply.github.com>
2023-06-12 09:22:12 +0530

    answer anagram.js

--- 01-js/easy/anagram.js
+++ 01-js/easy/anagram.js
@@ -8,7 +8,37 @@
 */
 
 function isAnagram(str1, str2) {
+  length1 = str1.length;
+  length2 = str2.length;
 
+  for (i = 0; i < length1; i++){
+        b = true;
+        for (j = 0; j < length2; j++){
+            if (str1[i] == str2[j]) {
+                b = false;
+                break;
+            }
+        }
+        if (b) {
+            return false;
+        }
+    }
+
+    for (i = 0; i < length2; i++) {
+        b = true;
+        for (j = 0; j < length1; j++) {
+            if (str2[i] == str1[j]) {
+                b = false;
+                break;
+            }
+        }
+        if (b) {
+            return false;
+        }
+    }
+
+
+    return true;
 }
 
 module.exports = isAnagram;
